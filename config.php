0f3d27a9a69b59268f69784a4609ea6974d8bedf
جاسم حوتی <59818587+jasemhooti@users.noreply.github.com>
2026-04-25 04:10:03 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
 
-define('SITE_URL', 'instanew-app.ir'); // آدرس سایت خودت
+define('SITE_URL', 'hast.jasemhooti2.ir'); // آدرس سایت خودت
 define('ADMIN_CARD', '6219861934798843');
 
 define('PANEL_URL', 'https://panil.jasemhooti2.ir/panel');
@@ -10,8 +10,8 @@ define('PANEL_PASSWORD', '725019516663486727');
 
 // تنظیمات دیتابیس (تغییر بده)
 $db_host = 'localhost';
-$db_name = 'instane5_jasemdb';
-$db_user = 'instane5_jasemuser2';
+$db_name = 'instane4_jasemdb';
+$db_user = 'instane4_jasemuser';
 $db_pass = 'Mm09370126906';
 
 try {
